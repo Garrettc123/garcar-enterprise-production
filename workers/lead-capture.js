0a7a3984c7088e0ebb3336ae0c799df5dd057bc9
Garrett Carrol <70427860+Garrettc123@users.noreply.github.com>
2026-07-30 23:21:29 -0500

    Allow GET in Cloudflare Worker CORS for health checks

--- workers/lead-capture.js
+++ workers/lead-capture.js
@@ -11,7 +11,7 @@
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Content-Type": "application/json",
 };
